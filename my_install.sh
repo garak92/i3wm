ad958362ca3b64ffc9c8c63a1ec9fe7391089c1e
Elim Garak <garak92@protonmail.com>
2021-04-04 01:23:36 -0400

    Notes for myself

--- my_install.sh
+++ my_install.sh
@@ -3,4 +3,9 @@
 #For my i3wm setup only, the dependencies are: picom imagemagick xss-lock i3lock rofi nitrogen autorandr
 #Autorandr is used because that allows me to turn off the laptop display automatically when my external hdmi monitor is plugged
 
+#Notes for myself: you also have to compile yay and do:
+#yay -S rust-analyzer and ycmd-git
+#Also, dont forget to install rustup (from the webpage), plug (from github), CocInstall (using Plug), and Coc-Rust-Analyzer (using CocInstall)
+
+
 sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim
