bec33e744e3fbee8a16705a2f670465f9a348641
Elim Garak <garak92@protonmail.com>
2021-04-04 01:26:38 -0400

    forgot to add awesome-terminal-fonts

--- my_install.sh
+++ my_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #This script autoinstalls everything I need on a fresh install, for my convenience
-#For my i3wm setup only, the dependencies are: picom imagemagick xss-lock i3lock rofi nitrogen autorandr
+#For my i3wm setup only, the dependencies are: picom imagemagick xss-lock i3lock rofi nitrogen autorandr awesome-terminal-fonts
 #Autorandr is used because that allows me to turn off the laptop display automatically when my external hdmi monitor is plugged
 
 #Notes for myself: you also have to compile yay and do:
@@ -8,4 +8,4 @@
 #Also, dont forget to install rustup (from the webpage), plug (from github), CocInstall (using Plug), and Coc-Rust-Analyzer (using CocInstall)
 
 
-sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim
+sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr awesome-terminal-fonts lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim
