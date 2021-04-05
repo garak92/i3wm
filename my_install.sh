baed00f2dec69c86bff3ff3e2f697d9e39787e37
Elim Garak <garak92@protonmail.com>
2021-04-05 08:05:28 -0400

    Some notes about picom and networking (wifi), awesome font version updated on installer, added bluetooth stuff on installer

--- my_install.sh
+++ my_install.sh
@@ -7,5 +7,7 @@
 #yay -S rust-analyzer and ycmd-git
 #Also, dont forget to install rustup (from the webpage), plug (from github), CocInstall (using Plug), and Coc-Rust-Analyzer (using CocInstall)
 
+#Networking (just in case): install dhcpd netctl wpa_supplicant, bring down interface using ip link device wl01 down and create a profile using wifi-menu
 
-sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr awesome-terminal-fonts lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim
+
+sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr otf-font-awesome lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim bluez blueman
