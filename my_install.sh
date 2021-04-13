8a61a49b2417dc183ca0f492634e40f6e2b53d81
Elim Garak <garak92@protonmail.com>
2021-04-13 11:28:18 -0400

    Brightness control added

--- my_install.sh
+++ my_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #This script autoinstalls everything I need on a fresh install, for my convenience
-#For my i3wm setup only, the dependencies are: picom imagemagick xss-lock i3lock rofi nitrogen autorandr awesome-terminal-fonts
+#For my i3wm setup only, the dependencies are: picom imagemagick xss-lock i3lock rofi nitrogen autorandr awesome-terminal-fonts brightnessctl
+
 #Autorandr is used because that allows me to turn off the laptop display automatically when my external hdmi monitor is plugged
 
 #Notes for myself: you also have to compile yay and do:
@@ -10,4 +11,4 @@
 #Networking (just in case): install dhcpd netctl wpa_supplicant, bring down interface using ip link device wl01 down and create a profile using wifi-menu
 
 
-sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr otf-font-awesome lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim bluez blueman
+sudo pacman -S picom imagemagick xss-lock i3lock rofi nitrogen autorandr otf-font-awesome lutris steam libreoffice-fresh galculator nvidia nvidia-prime htop pavucontrol vim bluez blueman brightnessctl
